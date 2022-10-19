0ee8c1275ee113f826a733c188396a477091545c
glozanoa <glozanoac@gmail.com>
2022-10-19 12:39:34 -0500

    fixing bug (just to test github webhook)

--- src/test/java/pl/jojczykp/jenkins_pipeline/ServiceTest.java
+++ src/test/java/pl/jojczykp/jenkins_pipeline/ServiceTest.java
@@ -20,7 +20,7 @@ class ServiceTest {
     void someMaths() {
         int x = 1;
         int y = 2
-        int result = 2; // FIX ME PLEASE! (THIS TEST BREAKE THE PIPELINE) 
+        int result = 3; // FIX ME PLEASE! (THIS TEST BREAKE THE PIPELINE) 
 
         assertEquals(x+y, result);
     }
